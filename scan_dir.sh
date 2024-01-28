45859d0527fee43322187009112092e298b8f792
Matthias Lee <matthias@matthiasclee.com>
2024-01-28 14:12:51 -0800

    Misc

--- scan_dir.sh
+++ scan_dir.sh
@@ -1,5 +1,7 @@
 TARGET_DIR=$1
-TARGET_DIR_FILES=($(ls $TARGET_DIR))
+TARGET_DIR_FILES=$(find $TARGET_DIR -maxdepth 1 -type f)
+TARGET_DIR_DIRECTORIES=$(find $TARGET_DIR -mindepth 1 -maxdepth 1 -type d)
 
 for file in "${TARGET_DIR_FILES[@]}"; do
+  du -s $file
 done
