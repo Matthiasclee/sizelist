a20dbb692548a972c3330565d6f867fc23135fb7
Matthias Lee <matthias@matthiasclee.com>
2024-01-28 20:02:17 -0800

    Misc

--- scan_dir.sh
+++ scan_dir.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
 TARGET_DIR=$1
-TARGET_DIR_FILES=$(find $TARGET_DIR -maxdepth 1 -type f)
-TARGET_DIR_DIRECTORIES=$(find $TARGET_DIR -mindepth 1 -maxdepth 1 -type d)
+
+readarray TARGET_DIR_FILES <<< "$(find "$TARGET_DIR" -maxdepth 1 -type f)"
+
+readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth 1 -type d)"
 
 for file in "${TARGET_DIR_FILES[@]}"; do
-  du -s $file
+  du -s "${file::-1}" 2> /dev/null | sort -gr
+done
+
+for file in "${TARGET_DIR_DIRECTORIES[@]}"; do
+  du -s "${file::-1}" 2> /dev/null | sort -gr
 done
