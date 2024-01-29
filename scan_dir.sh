8e73ef9c22f7c19508899d20d0f9699ff07dff88
Matthias Lee <matthias@matthiasclee.com>
2024-01-28 20:08:06 -0800

    Misc

--- scan_dir.sh
+++ scan_dir.sh
@@ -2,9 +2,11 @@
 
 TARGET_DIR=$1
 
-readarray TARGET_DIR_FILES <<< "$(find "$TARGET_DIR" -maxdepth 1 -type f)"
+cd $TARGET_DIR
 
-readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth 1 -type d)"
+readarray TARGET_DIR_FILES <<< "$(find "$TARGET_DIR" -maxdepth 1 -type f -exec realpath --relative-to="$(pwd)" {} \;)"
+
+readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth 1 -type d -exec realpath --relative-to="$(pwd)" {} \;)"
 
 for file in "${TARGET_DIR_FILES[@]}"; do
   du -s "${file::-1}" 2> /dev/null | sort -gr
