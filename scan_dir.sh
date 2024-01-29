af6576084152cf124d92408feb6b2fdb2385e3dd
Matthias Lee <matthias@matthiasclee.com>
2024-01-28 20:40:34 -0800

    Misc

--- scan_dir.sh
+++ scan_dir.sh
@@ -13,20 +13,20 @@ readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth
 for file in "${TARGET_DIR_FILES[@]}"; do
   du_output=$(du -sh "${file::-1}" 2> /dev/null)
   FILE_SIZES="$FILE_SIZES*$(cut -d ' ' -f 1 <(echo $du_output))"
-  FILE_NAMES="$FILE_NAMES*$(cut -d ' ' -f 2 <(echo $du_output))"
+  FILE_NAMES="$FILE_NAMES*$(cut -d ' ' -f 2- <(echo $du_output))"
 done
 
 for dir in "${TARGET_DIR_DIRECTORIES[@]}"; do
   du_output=$(du -sh "${dir::-1}" 2> /dev/null)
   DIR_SIZES="$DIR_SIZES*$(cut -d ' ' -f 1 <(echo $du_output))"
-  DIR_NAMES="$DIR_NAMES*$(cut -d ' ' -f 2 <(echo $du_output))"
+  DIR_NAMES="$DIR_NAMES*$(cut -d ' ' -f 2- <(echo $du_output))"
 done
 
-FILE_SIZES="${FILE_SIZES:1}"
-FILE_NAMES="${FILE_NAMES:1}"
+FILE_SIZES="FS:${FILE_SIZES:1}"
+FILE_NAMES="FN:${FILE_NAMES:1}"
 
-DIR_SIZES="${DIR_SIZES:1}"
-DIR_NAMES="${DIR_NAMES:1}"
+DIR_SIZES="DS:${DIR_SIZES:1}"
+DIR_NAMES="DN:${DIR_NAMES:1}"
 
 echo $FILE_SIZES
 echo $FILE_NAMES
