324b155095ed96c93cfeb9dcae6c66e1e88b3977
Matthias Lee <matthias@matthiasclee.com>
2024-01-29 19:38:55 -0800

    Misc

--- main.sh
+++ main.sh
@@ -9,4 +9,10 @@ IFS="*" read -a FILE_NAMES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[1]::
 IFS="*" read -a DIR_SIZES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[2]::-1}"))"
 IFS="*" read -a DIR_NAMES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[3]::-1}"))"
 
+DIR_NAMES_ALL=""
 
+for index in "${!DIR_NAMES[@]}"; do
+  DIR_NAMES_ALL="$DIR_NAMES_ALL\n${DIR_SIZES[$index]} ${DIR_NAMES[$index]}"
+done
+
+echo -e "$DIR_NAMES_ALL" | sort -hr
--- scan_dir.sh
+++ scan_dir.sh
@@ -6,20 +6,24 @@ TARGET_DIR=$1
 cd $TARGET_DIR
 
 # Get lists of files
-readarray TARGET_DIR_FILES <<< "$(find "$TARGET_DIR" -maxdepth 1 -type f -printf '%P\n')"
-readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth 1 -type d -printf '%P\n')"
+readarray TARGET_DIR_FILES <<< "$(find "$TARGET_DIR" -maxdepth 1 -type f -printf '"%P"\n')"
+readarray TARGET_DIR_DIRECTORIES <<< "$(find "$TARGET_DIR" -mindepth 1 -maxdepth 1 -type d -printf '"%P"\n')"
+
+TARGET_DIR_FILES="$(echo "${TARGET_DIR_FILES[@]}" | tr "\n" " ")"
 
 # Get file sizes
-for file in "${TARGET_DIR_FILES[@]}"; do
-  du_output=$(du -sh "${file::-1}" 2> /dev/null)
-  FILE_SIZES="$FILE_SIZES*$(cut -d ' ' -f 1 <(echo $du_output))"
-  FILE_NAMES="$FILE_NAMES*$(cut -d ' ' -f 2- <(echo $du_output))"
+readarray du_output <<< "$(bash -c "du -sh $TARGET_DIR_FILES 2> /dev/null" | sort -hr)"
+for file in "${du_output[@]}"; do
+  FILE_SIZES="$FILE_SIZES*$(cut -d ' ' -f 1 <(echo $file))"
+  FILE_NAMES="$FILE_NAMES*$(cut -d ' ' -f 2- <(echo $file))"
 done
 
-for dir in "${TARGET_DIR_DIRECTORIES[@]}"; do
-  du_output=$(du -sh "${dir::-1}" 2> /dev/null)
-  DIR_SIZES="$DIR_SIZES*$(cut -d ' ' -f 1 <(echo $du_output))"
-  DIR_NAMES="$DIR_NAMES*$(cut -d ' ' -f 2- <(echo $du_output))"
+TARGET_DIR_DIRECTORIES="$(echo "${TARGET_DIR_DIRECTORIES[@]}" | tr "\n" " ")"
+
+readarray du_output <<< "$(bash -c "du -sh $TARGET_DIR_DIRECTORIES 2> /dev/null" | sort -hr)"
+for directory in "${du_output[@]}"; do
+  DIR_SIZES="$DIR_SIZES*$(cut -d ' ' -f 1 <(echo $directory))"
+  DIR_NAMES="$DIR_NAMES*$(cut -d ' ' -f 2- <(echo $directory))"
 done
 
 FILE_SIZES="FS:${FILE_SIZES:1}"
