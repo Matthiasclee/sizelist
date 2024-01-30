482f5a88cbfea6a56001fb5b7fad87817146210b
Matthias Lee <matthias@matthiasclee.com>
2024-01-29 19:06:58 -0800

    Use arrays

--- main.sh
+++ main.sh
@@ -4,8 +4,9 @@ TARGET_DIR=$1
 
 readarray dir_scan_output <<< "$(bash ./scan_dir.sh $TARGET_DIR)"
 
-FILE_SIZES="$(cut -d ':' -f 2 <(echo "${dir_scan_output[0]::-1}"))"
-FILE_NAMES="$(cut -d ':' -f 2 <(echo "${dir_scan_output[1]::-1}"))"
-DIR_SIZES="$(cut -d ':' -f 2 <(echo "${dir_scan_output[2]::-1}"))"
-DIR_NAMES="$(cut -d ':' -f 2 <(echo "${dir_scan_output[3]::-1}"))"
+IFS="*" read -a FILE_SIZES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[0]::-1}"))"
+IFS="*" read -a FILE_NAMES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[1]::-1}"))"
+IFS="*" read -a DIR_SIZES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[2]::-1}"))"
+IFS="*" read -a DIR_NAMES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[3]::-1}"))"
+
 
