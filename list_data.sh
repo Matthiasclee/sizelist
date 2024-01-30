d2769b5621a2bb156de4d66bae3a552b2a1b88e7
Matthias Lee <matthias@matthiasclee.com>
2024-01-29 20:52:44 -0800

    It works

--- list_data.sh
+++ list_data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 TARGET_DIR=$1
+script_dir=$2
 
-readarray dir_scan_output <<< "$(bash ./scan_dir.sh $TARGET_DIR)"
+readarray dir_scan_output <<< "$(bash "$script_dir/scan_dir.sh" "$TARGET_DIR")"
 
 IFS="*" read -a FILE_SIZES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[0]::-1}"))"
 IFS="*" read -a FILE_NAMES <<< "$(cut -d ':' -f 2 <(echo "${dir_scan_output[1]::-1}"))"
@@ -21,6 +22,8 @@ for index in "${!FILE_NAMES[@]}"; do
   FILE_NAMES_ALL="$FILE_NAMES_ALL\n${FILE_SIZES[$index]} ${FILE_NAMES[$index]}"
 done
 
+echo "* $(pwd)"
+echo "* Back"
 echo "* Exit"
 echo -e "$DIR_NAMES_ALL" | sort -hr
 echo -e "$FILE_NAMES_ALL" | sort -hr
--- main.sh
+++ main.sh
@@ -2,13 +2,19 @@
 
 TARGET_DIR=$1
 
+script_dir="$(dirname "$(readlink -f "$0")")"
+
+cd "$TARGET_DIR"
+
 while true; do
-  SELECTION=$(bash ./list_data.sh $TARGET_DIR | fzf --reverse)
+  TARGET_DIR="."
+  SELECTION=$(bash "$script_dir/list_data.sh" "$TARGET_DIR" "$script_dir" | fzf --reverse)
   if [ "$SELECTION" == "* Exit" ]; then
     break
+  elif [ "$SELECTION" == "* Back" ]; then
+    cd ..
   else
     TARGET_DIR=$(echo "$SELECTION" | cut -d ' ' -f 2-)
     cd $TARGET_DIR
-    TARGET_DIR="."
   fi
 done
